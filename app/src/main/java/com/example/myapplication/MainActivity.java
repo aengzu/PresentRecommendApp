20cb6b1c812b0eece6fa20b1c2cf54b298fffdcf
anegzu <jeon7879@naver.com>
2023-05-05 15:43:16 +0900

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.myapplication;
 
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,17 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
 
 
-        Button go_sign_btn = (Button) findViewById(R.id.go_sign);
-        go_sign_btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SIgn.class);
-                startActivity(intent);
-            }
-        });
     }
 }
