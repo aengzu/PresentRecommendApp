2fb653d2d266d5bd4db1dfca50e13bc64e8a6406
aengzu <jeon7879@naver.com>
2023-05-23 10:15:30 +0900

    "메인액티비티 버튼이벤트"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -18,12 +19,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button test = (Button) findViewById(R.id.view8);
+        ImageButton gift_box = (ImageButton) findViewById(R.id.box_img);
+        Button ranking_box = (Button) findViewById(R.id.ranking_btn);
+        Button heart_box = (Button) findViewById(R.id.heart_btn);
+        Button bg_box = (Button) findViewById(R.id.bg_btn);
+        Button mg_box = (Button) findViewById(R.id.mg_bt);
 
-        test.setOnClickListener(new View.OnClickListener() {
+
+
+        //선물 박스 클릭 시 페이지 이동 이벤트
+        gift_box.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "친구목록으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Friend_list.class);
+                startActivity(intent);
+            }
+        });
+
+        // 미니게임 클릭 시 페이지 이동 이벤트
+        gift_box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Friend_list.class);
+                startActivity(intent);
+            }
+        });
+
+        // 랭킹 클릭 시 페이지 이동 이벤트
+        gift_box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Friend_list.class);
+                startActivity(intent);
+            }
+        });
+
+        // 찜목록 클릭 시 페이지 이동 이벤트
+        gift_box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Friend_list.class);
                 startActivity(intent);
             }
