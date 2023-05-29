2cd2e3ad49c5ea9c2bb046f51b1cf677bdeed761
TEARFEAR <cigul12@icloud.com>
2023-05-29 13:41:08 +0900

    mid fin

--- app/src/main/java/com/example/myapplication/gift_friend_n.java
+++ app/src/main/java/com/example/myapplication/gift_friend_n.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +9,21 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gift_friend_n extends AppCompatActivity {
+    Dialog dialog01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personalized_gift_not_friendlist);
+
+        dialog01 = new Dialog(gift_friend_n.this);
+        dialog01.setContentView(R.layout.game_popup);
+
+        findViewById(R.id.make_g).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog01(); // 아래 showDialog01() 함수 호출
+            }
+        });
 
         Button self = (Button) findViewById(R.id.self);
         Button make_g = (Button) findViewById(R.id.make_g);
