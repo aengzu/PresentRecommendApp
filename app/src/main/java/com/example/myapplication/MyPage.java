2e713696dd83958e228cbc8a0bdc37015e73696b
Yun-seoung <choikang0607@gmail.com>
2023-05-29 14:27:29 +0900

    마이페이지 수정

--- app/src/main/java/com/example/myapplication/MyPage.java
+++ app/src/main/java/com/example/myapplication/MyPage.java
@@ -6,6 +6,6 @@ public class MyPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mypage);
+        setContentView(R.layout.mytaste);
     }
 }
