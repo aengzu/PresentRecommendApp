93a0e0ff89f9d62b8d494a2e8f191cf0612102c7
hyupb <wsh030114@naver.com>
2023-05-31 16:32:30 +0900

    미니게임

--- app/src/main/java/com/example/myapplication/MiniGame.java
+++ app/src/main/java/com/example/myapplication/MiniGame.java
@@ -1,13 +1,24 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MiniGame extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btn_back;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.minigame);
+        btn_back=(Button)findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
