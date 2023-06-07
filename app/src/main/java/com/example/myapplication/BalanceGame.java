df627b40ae958414837ce93f1d1841f092787d73
TEARFEAR <cigul12@icloud.com>
2023-06-07 15:24:25 +0900

    back_btn fin

--- app/src/main/java/com/example/myapplication/BalanceGame.java
+++ app/src/main/java/com/example/myapplication/BalanceGame.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,14 @@ public class BalanceGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_popup);
+        Button del_btn=(Button)findViewById(R.id.del_btn);
+        del_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BalanceGame.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
